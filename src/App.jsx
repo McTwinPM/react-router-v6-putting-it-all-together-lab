6af5e45453b56e83317c1b54373b47f80536b34b
Michael McCormack <mcctwin@gmail.com>
2025-08-16 15:15:13 -0500

    worked on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import DirectorContainer from "./pages/DirectorContainer"
+import DirectorCard from "./pages/DirectorCard"
+import DirectorForm from "./pages/DirectorForm"
+import DirectorList from "./pages/DirectorList"
+import MovieCard from "./pages/MovieCard"
+import MovieForm from "./pages/MovieForm"
+
 const App = () => {
     return (
-        <>
-        {/* define all routes here */}
-        </>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/directors" element={<DirectorContainer />}>
+                <Route index element={<DirectorList />} />
+                <Route path="new" element={<DirectorForm />} />
+                <Route path=":id" element={<DirectorCard />} />
+            </Route>
+            <Route path="/movies" element={<MovieContainer />}>
+                <Route index element={<MovieList />} />
+                <Route path="new" element={<MovieForm />} />
+                <Route path=":id" element={<MovieCard />} />
+            </Route>
+        </Routes>
+        </BrowserRouter>
     )
 }
 
